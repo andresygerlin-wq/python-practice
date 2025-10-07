c90585b33e471cb25e1bc7c8ddf2c084f5a70e4d
andresygerlin-wq <andres.y.gerlin@gmail.com>
2025-10-07 02:57:43 -0600

    Add basic functions practice

---
+++ 01 - basics/functions/basic_functions.py
@@ -0,0 +1,12 @@
+# My Python Functions Practice
+# Basic function exercises
+
+def greet(name):
+  return f"Hello {name}!"
+
+def add(a, b):
+  return a + b
+
+#Test the functions
+print(greet("Andres"))
+print(add(5, 3))
